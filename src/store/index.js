b61c07f21f77feaf6d96b903d232e431eecffcb2
Pedro Mass <pedro.j.mass@gmail.com>
2018-02-03 14:50:16 -0500

    Split out store config from Store component.

--- src/store/index.js
+++ src/store/index.js
@@ -1,33 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
 
-import reducers from './reducers';
+import configureStore from './config';
 
 export default function Store(props) {
-  return <Provider store={store}>{props.children}</Provider>;
+  return <Provider store={configureStore({})}>{props.children}</Provider>;
 }
 
 Store.propTypes = {
   children: PropTypes.node
 };
-
-// ------- config -------
-
-// creates the logger only for dev
-const loggerMiddleware = createLogger({
-  predicate: () => __DEV__
-});
-
-function configureStore(initialState) {
-  const enhancers = compose(applyMiddleware(thunkMiddleware, loggerMiddleware));
-
-  const reducer = combineReducers(reducers);
-
-  return createStore(reducer, initialState, enhancers);
-}
-
-const store = configureStore({});
